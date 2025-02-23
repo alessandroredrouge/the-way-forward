de8d257ff0a8ccfccc13255d942e357871cccfe6
alessandroredrouge <alessandroredrouge@gmail.com>
2025-02-23 16:59:55 +0100

    commented unused endpoints

--- backend/app/api/v1/api.py
+++ backend/app/api/v1/api.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import ideas, users, podcasts, resources
+from app.api.v1.endpoints import ideas
 
 api_router = APIRouter()
 
 api_router.include_router(ideas.router, prefix="/ideas", tags=["ideas"])
-api_router.include_router(users.router, prefix="/users", tags=["users"])
-api_router.include_router(podcasts.router, prefix="/podcasts", tags=["podcasts"])
-api_router.include_router(resources.router, prefix="/resources", tags=["resources"]) 
+# api_router.include_router(users.router, prefix="/users", tags=["users"])
+# api_router.include_router(podcasts.router, prefix="/podcasts", tags=["podcasts"])
+# api_router.include_router(resources.router, prefix="/resources", tags=["resources"]) 
